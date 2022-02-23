bd3022f2ce22acfe837ca3d905216ebb77162cfc
satyaveer1994 <satyaveerjat1994@gmail.com>
2022-02-23 21:36:23 +0530

    assignment2

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,57 +2,101 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/students/:name", function (req, res) {
-  let studentName = req.params.name
+  let studentName = req.params.name;
   console.log(studentName);
   res.send(studentName);
-})
+});
 //
 router.get("/movies", function (req, res) {
-  const movies =('["Fukrey","Delhi","Dabang","Rockstar","Suryawansh"]')
-res.send(movies);
+  const movies = '["Fukrey","Delhi","Dabang","Rockstar","Suryawansh"]';
+  res.send(movies);
 });
 //
 router.get("/movies/:index", function (req, res) {
-  const movie = ["Fukrey", "Delhi", "Dabang", "Rockstar", "Suryawansh"];
-  let value = req.parama.movieId
-  if (index>movies.length) {
+  const index = req.params.index;
+  const movies = ["Fukrey", "Delhi", "Dabang", "Rockstar", "Suryawansh"];
+
+  if (index > movies.length) {
     res.send("use a valid index");
   } else {
-    res.send(movies[index-1]);
+    res.send(movies[index -1]);
   }
-})
+});
 //
 
-router.get('/film',function(req,res){
-  
-  const film =([{id:1,name:'The Shining'},{id:2,name:'Incendies'},{id:3,name:'Rang de Basanti'},{id:4,name:'puspa'}])
-  res.send(film);
+
+
+router.get("/films", function (req, res) {
+  const arrayOfFilms = [
+    {
+      id: 1,
+      name: "Dune",
+    },
+    {
+      id: 2,
+      name: "Wall Street",
+    },
+    {
+      id: 3,
+      name: "Big Short",
+    },
+    {
+      id: 4,
+      name: "3 ediot",
+    },
+  ];
+  res.send(arrayOfFilms);
 });
 
+router.get("/films/:filmId", function (req, res) {
+  const arrayOfFilms = [
+    {
+      id: 1,
+      name: "Dune",
+    },
+    {
+      id: 2,
+      name: "Wall Street",
+    },
+    {
+      id: 3,
+      name: "Big Short",
+
+      
+    },
+    {
+      id: 4,
+      name: "3 ediot",
+    },
+  ];
+
+  let value = req.params.filmId;
+  let found = false;
+  for (let i = 0; i < arrayOfFilms.length; i++) {
+    if (arrayOfFilms[i].id == value) {
+      found = true;
+      res.send(arrayOfFilms[i]);
+      break;
+    }
+  }
+  if (found == false) {
+    res.send("No movie with given index exists!");
+  }
+});
+
+module.exports=router;
+// router.get("/sol1",function (req,res){
+
+// let numArray = [1,2,3,5,6,7]
+// let n = 1+(numArray.length)
+// let realSum = (n*(n+1))/2
+// let diffSum = 0;
+// for (i=0; i<numArray.length; i++){
+//     diffSum += numArray[i];
+// }
+// let missingNum = realSum - diffSum;
+// console.log('the missing number is : ', missingNum)
+// res.send(missing)
+// });
 
 
-
-
-
-//
-
-router.get('/films/:filmId',function (req,res){
-  const filmId=req.params.filmId
-    const filmId= [{Id: 1,name:'The Shining'},{Id: 2, name:'Incendies'},{Id: 3, name:'Rang de Basanti'},{Id:4, name:'puspa'}]
-  
-if (filmId>film.length){
-  res.send("No movie exists with this id")
-}else{
-  res.send(film[filmId-1]);
-}
-
-
-
-
-    });
-
-
-
-
-
-module.router.express;
